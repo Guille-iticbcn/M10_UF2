38b775417429bb0c8d3d0caa52c8b6f07d0974fb
guilleee50 <04guillejm@gmail.com>
2024-01-11 17:32:25 +0100

    DIA 2

--- Repas/exercici10.py
+++ Repas/exercici10.py
@@ -1,5 +1,17 @@
 import random
 
 num = random.randint(0,100)
+sapElNum = False
+numIntents = 0
 
-print(num)
+while (sapElNum == False):
+    adivina = int(input("Entra el numero: "))
+    if(adivina > num):
+        print(f'El número és més petit que {adivina}.')
+        numIntents += 1
+    elif (adivina < num):
+        print(f'El número és més gran que {adivina}.')
+        numIntents += 1
+    else:
+        print(f'Has adivinat el número! Ho has intentat {numIntents} vegades.')
+        sapElNum = True
