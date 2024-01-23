da876a071731ea134ae7a27962aca81a5a52a9e1
Guille-iticbcn <2023_guillermo.jaume@iticbcn.cat>
2024-01-23 17:22:45 +0100

    .

--- Repas/exercici7.py
+++
@@ -1,6 +0,0 @@
-i = 0
-
-while i < 151:
-    if (i % 2 != 0):
-        print(i)
-    i += 1
--- Repas/exercici8.py
+++
@@ -1,4 +0,0 @@
-valor = input("Introdueix entre 1 i 3 paraules: ")
-print(f'Primera lletra: {valor[0]}')
-print(f'Última lletra: {valor[len(valor) - 1]}')
-print(f'Número de caràcters: {len(valor)}')
