2ad240f756635eadb6552b008c99726db9b996f2
guilleee50 <04guillejm@gmail.com>
2024-02-04 17:51:14 +0100

    UPDATE main

--- practica_2/main.py
+++ practica_2/main.py
@@ -4,21 +4,21 @@ from read import *
 from delete import *
 from update import *
 
+#AQUEST ARXIU CREA UN MENÚ I CRIDA A LES FUNCIONS D'ALTRES ARXIUS PER FER EL CRUD
+
+#FUNCIONS DEL MENÚ
 def mostrar_menu(opciones):
     print('Selecciona una opció:')
     for clave in sorted(opciones):
         print(f' {clave}) {opciones[clave][0]}')
-
 
 def leer_opcion(opciones):
     while (a := input('Opció: ')) not in opciones:
         print('Opció incorrecta, torni a intentar-ho.')
     return a
 
-
 def ejecutar_opcion(opcion, opciones):
     opciones[opcion][1]()
-
 
 def generar_menu(opciones, opcion_salida):
     opcion = None
@@ -28,7 +28,7 @@ def generar_menu(opciones, opcion_salida):
         ejecutar_opcion(opcion, opciones)
         print()
 
-
+#CONFIGURACIÓ DEL MENÚ
 def menu_principal():
     opciones = {
         '1': ('CREATE TABLE', createTable),
@@ -38,17 +38,11 @@ def menu_principal():
         '5': ('DELETE VEHICLE', delete),
         '6': ('Sortir', salir)
     }
-
     generar_menu(opciones, '6')
 
 
 def salir():
     print('Sortint')
-
-
-def connectionExecte(sql):
-    connection.execute(sql)
-    conn.commit()
 
 
 if __name__ == '__main__':
